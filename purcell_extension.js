ff535bcf1f16759c8132bbf1f5705fe20aa91512
Mike Solomon <mike@mikesolomon.org>
2014-09-25 08:04:37 +0200

    Edits to make the site operational.

--- purcell_extension.js
+++ purcell_extension.js
@@ -1,3 +1,6 @@
+function update_loading(n) {
+  $('#loading_text').text("[loading "+n+"\%]");
+}
 function make_purcell_header() {
 	    var out = [];
 	    out.push({sql:"INSERT INTO dot_padding (id, val)  VALUES (-1,1.0);"});
@@ -257,6 +260,70 @@ function make_complex_example(out) {
     out.push({sql:"INSERT INTO graphical_next (id, prev, next) VALUES (20,19,NULL);"});
     return out;
 }
+
+function make_beamed_note(out, n,b,p,o) {
+    out.push({sql:"INSERT INTO name (id, val)  VALUES ("+n+",'note');"});
+    out.push({sql:"INSERT INTO font_name (id, val)  VALUES ("+n+",'Bravura');"});
+    out.push({sql:"INSERT INTO font_size (id, val)  VALUES ("+n+",20);"});
+    out.push({sql:"INSERT INTO duration_log (id, val)  VALUES ("+n+",-4);"});
+    out.push({sql:"INSERT INTO staff_symbol (id, val)  VALUES ("+n+",1);"});
+    out.push({sql:"INSERT INTO pitch (id, val)  VALUES ("+n+","+p+");"});
+    out.push({sql:"INSERT INTO octave (id, val)  VALUES ("+n+","+o+");"});
+    out.push({sql:"INSERT INTO beam (id, val)  VALUES ("+n+","+b+");"});
+    out.push({sql:"INSERT INTO used_ids (id)  VALUES ("+n+");"});
+    return out
+}
+
+function make_graphical_nexts(out, gns) {
+  for(var i = 1; i < gns.length - 1; i++) {
+    out.push({sql:"INSERT INTO graphical_next (id, prev, next) VALUES ("+gns[i]+","+gns[i-1]+","+gns[i+1]+");"});
+  }
+  return out;
+}
+
+function make_scale_example(out) {
+    out.push({sql:"DELETE FROM graphical_next;"});
+    out.push({sql:"INSERT INTO used_ids (id)  VALUES (3);"});
+    out = make_beamed_note(out, 4,3,1,0)
+    out = make_beamed_note(out, 5,3,2,0)
+    out = make_beamed_note(out, 6,3,3,0)
+    out = make_beamed_note(out, 7,3,4,0)
+
+    out.push({sql:"INSERT INTO used_ids (id)  VALUES (8);"});
+    out = make_beamed_note(out, 9,8,1,0)
+    out = make_beamed_note(out, 10,8,2,0)
+    out = make_beamed_note(out, 11,8,1,1)
+    out = make_beamed_note(out, 12,8,2,1)
+
+    out.push({sql:"INSERT INTO used_ids (id)  VALUES (13);"});
+    out = make_beamed_note(out, 14,13,3,0)
+    out = make_beamed_note(out, 15,13,4,0)
+    out = make_beamed_note(out, 16,13,1,1)
+    out = make_beamed_note(out, 17,13,2,1)
+
+    out.push({sql:"INSERT INTO used_ids (id)  VALUES (18);"});
+    out = make_beamed_note(out, 19,18,3,0)
+    out = make_beamed_note(out, 20,18,4,0)
+    out = make_beamed_note(out, 21,18,3,1)
+    out = make_beamed_note(out, 22,18,4,1)
+
+    out.push({sql:"INSERT INTO name (id, val)  VALUES (23,'bar_line');"});
+    out.push({sql:"INSERT INTO bar_thickness (id, val)  VALUES (23,0.20);"});
+    out.push({sql:"INSERT INTO staff_symbol (id, val)  VALUES (23,1);"});
+
+    out = make_graphical_nexts(out,['NULL',2,0,4,5,6,7,9,10,11,12,14,15,16,17,19,20,21,22,23,'NULL']);
+    return out;
+}
+
+
+function array_to_single_sql(out) {
+  big = "";
+  for (var i = 0; i < out.length; i++) {
+    big += out[i].sql+'\n';
+  }
+  
+  return [{sql:big}];
+}
 function processSql_page5(data) {
      console.log("received", data);
      //console.log("DO", data.information);
@@ -273,6 +340,7 @@ function processSql_page5(data) {
      purcell.$e$$ion$.page4.draw(data);
 }
 function interpret_query_page5() {
+  purcell.$e$$ion$.page4.make_spin();
   var out = [{name:"information", sql :
   $("#sql_input_page5").val()}];
   purcell.append_standard_graphical_queries(out);
@@ -303,6 +371,7 @@ function processSql_page6(data) {
      purcell.$e$$ion$.page4.draw(data);
 }
 function interpret_query_page6() {
+  purcell.$e$$ion$.page4.make_spin();
   var out = [{name:"information", sql :
   $("#sql_input_page6").val()}];
   purcell.append_standard_graphical_queries(out);
@@ -333,6 +402,7 @@ function processSql_page7(data) {
      purcell.$e$$ion$.page4.draw(data);
 }
 function interpret_query_page7() {
+  purcell.$e$$ion$.page4.make_spin();
   var out = [{name:"information", sql :
   $("#sql_input_page7").val()}];
   purcell.append_standard_graphical_queries(out);
@@ -343,4 +413,265 @@ function interpret_query_page7() {
     subsequent:"processSql_page7"
   };
   purcell.$e$$ion$.page4.$0cket.send(JSON.stringify(out));
+}
+
+/////////////
+
+function processSql_simple_editor(data) {
+     console.log("received", data);
+     //console.log("DO", data.information);
+     var stringified = JSON.stringify(data.information,
+     function(key, val) {
+         if (val == null) {return val; }
+         return val.toFixed ? Number(val.toFixed(3)) : val;
+         }, 2);
+     if (stringified != '[]') {
+       $('#sql_result_simple_editor').text(stringified);
+     } else {
+       $('#sql_result_simple_editor').text('');
+     }
+     purcell.$e$$ion$.simple_editor.draw(data);
+}
+function interpret_query_simple_editor() {
+  var out = [{name:"information", sql :
+  $("#sql_input_simple_editor").val()}];
+  purcell.append_standard_graphical_queries(out);
+  out = {
+    client:purcell.$e$$ion$.simple_editor.MY_NAME,
+    sql:out,
+    'return': purcell._be(purcell.$e$$ion$.simple_editor.MY_NAME),
+    subsequent:"processSql_simple_editor"
+  };
+  purcell.$e$$ion$.simple_editor.$0cket.send(JSON.stringify(out));
+}
+
+/////////////
+
+function processSql_page8(data) {
+     console.log("received", data);
+     //console.log("DO", data.information);
+     var stringified = JSON.stringify(data.information,
+     function(key, val) {
+         if (val == null) {return val; }
+         return val.toFixed ? Number(val.toFixed(3)) : val;
+         }, 2);
+     if (stringified != '[]') {
+       $('#sql_result_page8').text(stringified);
+     } else {
+       $('#sql_result_page8').text('');
+     }
+     purcell.$e$$ion$.page8.draw(data);
+}
+function interpret_query_page8() {
+  var out = [{name:"information", sql :
+  $("#sql_input_page8").val()}];
+  purcell.append_standard_graphical_queries(out);
+  out = {
+    client:purcell.$e$$ion$.page8.MY_NAME,
+    sql:out,
+    'return': purcell._be(purcell.$e$$ion$.page8.MY_NAME),
+    subsequent:"processSql_page8"
+  };
+  purcell.$e$$ion$.page8.$0cket.send(JSON.stringify(out));
+}
+
+var _UPDATES = function() {
+  var ids = [4,5,6,7,9,10,11,12,14,15,16,17,19,20,21,22];
+  var oct = [1,1,1,1,1,1,0,0,1,1,0,0,1,1,0,0];
+  var out = [];
+  for (var i = 0; i < ids.length; i++) {
+    out.push("UPDATE octave SET val = "+oct[i]+" WHERE id = "+ids[i]);
+  }
+  for (var i = 0; i < ids.length; i++) {
+    out.push("UPDATE octave SET val = "+(oct[i] * -1 + 1)+" WHERE id = "+ids[i]);
+  }
+  return out;
+}();
+
+var _CRUNCH = 0;
+var _CURRENT = 0;
+var _REPERTOIRE = [];
+var _USEABLES = [];
+
+// ugh - code dup of draw function
+function crunch_data_to_useable() {
+
+  for (var rep_idx = 0; rep_idx < _REPERTOIRE.length; rep_idx++) {
+
+    var data = _REPERTOIRE[rep_idx];
+    var to_push_back = [];
+    if (data.line_stencil != null) {
+      for (var i = 0; i < data.line_stencil.length; i++) {
+        var line = data.line_stencil[i];
+        //console.log("making line", line);
+        var x = parseFloat(!purcell.is_null(line['x_position']) ? line['x_position'] : 0.0);
+        var y = parseFloat(!purcell.is_null(line['y_position']) ? line['y_position'] : 0.0);
+        var name = line['name'] ? line['name'] : '';
+        to_push_back.push(
+        {
+          id : name+'_line_'+line['id']+'_'+line['sub_id'],
+          type : 'line',
+          x1 : purcell.$e$$ion$.page8.st_x(parseFloat(line['x0']) + x),
+          y1 : purcell.$e$$ion$.page8.st_y(parseFloat(line['y0']) + y),
+          x2 : purcell.$e$$ion$.page8.st_x(parseFloat(line['x1']) + x),
+          y2 : purcell.$e$$ion$.page8.st_y(parseFloat(line['y1']) + y)
+        });
+      }
+    }
+    if (data.glyph_stencil != null) {
+      for (var i = 0; i < data.glyph_stencil.length; i++) {
+          var glyph = data.glyph_stencil[i];
+          //console.log("making glyph", glyph);
+          var x = parseFloat(glyph['x']) + parseFloat(!purcell.is_null(glyph['x_position']) ? glyph['x_position'] : 0.0);
+          var y = parseFloat(glyph['y']) + parseFloat(!purcell.is_null(glyph['y_position']) ? glyph['y_position'] : 0.0);
+          uc = String.fromCharCode(parseInt(glyph['unicode'].substr(2), 16));
+          var name = glyph['name'] ? glyph['name'] : '';
+          //console.log(context.font, glyph[3], uc);
+          to_push_back.push({
+            id : name+'_glyph_'+glyph['id']+'_'+glyph['sub_id'],
+            type : 'glyph',
+            x : purcell.$e$$ion$.page8.st_x(x),
+            y : purcell.$e$$ion$.page8.st_y(y),
+            unicode : uc
+          });
+      }
+    }
+    var polygon_holder = {};
+    if (data.polygon_stencil != null) {
+      for (var i = 0; i < data.polygon_stencil.length; i++) {
+        var polygon = data.polygon_stencil[i];
+        //console.log("PG",polygon);
+        if (!polygon_holder[polygon['id']]) {
+          polygon_holder[polygon['id']] = {};
+        }
+        if (!polygon_holder[polygon['id']][polygon['sub_id']]) {
+          //console.log("making sub", polygon['sub_id']);
+          polygon_holder[polygon['id']][polygon['sub_id']] = [];
+        }
+        polygon_holder[polygon['id']][polygon['sub_id']].push(polygon);
+      }
+      //console.log("PH", polygon_holder);
+      for (key in polygon_holder) {
+        for (sub_key in polygon_holder[key]) {
+          // first, we sort in point order
+          //console.log("before sorting", polygon_holder[key][sub_key])
+          polygon_holder[key][sub_key].sort(function(a,b)
+            {
+              return parseInt(a['point']) - parseInt(b['point']);
+            });
+          //console.log("after sorting", polygon_holder[key][sub_key])
+          // then, iterate
+          var path = "";
+          for (var i = 0; i < polygon_holder[key][sub_key].length; i++) {
+            path = path + ((i == 0 ? 'M' : 'L') + " " + purcell.$e$$ion$.page8.st_x(parseFloat(polygon_holder[key][sub_key][i].x)) + " " + purcell.$e$$ion$.page8.st_y(parseFloat(polygon_holder[key][sub_key][i].y))+ " ");
+          }
+          var name = polygon_holder[key][sub_key][0]['name'] ? polygon_holder[key][sub_key][0]['name'] : '';
+          var thick = parseFloat(polygon_holder[key][sub_key][0].thickness ? polygon_holder[key][sub_key][0].thickness : 0.0);
+          to_push_back.push({
+            id : name+'_polygon_'+polygon_holder[key][sub_key][0]['id']+'_'+polygon_holder[key][sub_key][0]['sub_id'],
+            type : 'path',
+            path : path,
+          });
+          //s_polygon.click(function() { return function() { purcell.$e$$ion$.page8.registerAsClicked(s_polygon) } });
+        }        
+      }
+    }
+    _USEABLES.push(to_push_back);
+  }
+  //console.log(_USEABLES);
+}
+
+function find_by_id(lst, id) {
+  for (var i = 0; i < lst.length; i++) {
+    //console.log(lst[i].id, lst[i].type);
+    if (lst[i].id == id) {
+      return lst[i];
+    }
+  }
+}
+
+function interpolate(s,e,t) {
+  return ((e - s) * t) + s;
+}
+
+function do_animation () {
+  var surface = purcell.$e$$ion$.page8.$urfaces[0];
+  var idx = _CURRENT;
+  _CURRENT += 1;
+  var destination = _USEABLES[idx % _USEABLES.length];
+  var start = _USEABLES[idx == 0 ? _USEABLES.length - 1 : ((idx - 1) % _USEABLES.length)]
+  Snap.animate(
+    0,
+    1.0,
+    function(val) {
+      for (var i = 0; i < destination.length; i++) {
+        var curr = destination[i];
+        var obj = surface.select('#'+curr.id);
+        var prev = find_by_id(start, curr.id);
+        //console.log(obj, curr.id)
+        if (curr.type == 'line') {
+          obj.attr({
+            x1: interpolate(prev.x1, curr.x1, val),
+            x2: interpolate(prev.x2, curr.x2, val),
+            y1: interpolate(prev.y1, curr.y1, val),
+            y2: interpolate(prev.y2, curr.y2, val),
+          });
+        } else if (curr.type == 'glyph') {
+          obj.attr({
+            x: interpolate(prev.x, curr.x, val),
+            y: interpolate(prev.y, curr.y, val),
+          });
+        } else if (curr.type == 'path') {
+          var prev_parsed = Snap.parsePathString(prev.path);
+          var curr_parsed = Snap.parsePathString(curr.path);
+          var path = '';
+          for (var j = 0; j < curr_parsed.length; j++) {
+            for (var k = 1; k < curr_parsed[j].length; k++) {
+              curr_parsed[j][k] = interpolate(prev_parsed[j][k], curr_parsed[j][k], val)
+            }
+            for (var k = 0; k < curr_parsed[j].length; k++) {
+              path += curr_parsed[j][k]+' ';
+            }
+          }
+          obj.attr({
+            path : path
+          });
+        }
+      }
+    },
+    1000,
+    mina.linear,
+    do_animation
+  );
+}
+
+function deposit_draw_repertoire(data) {
+     //console.log("ddr", data, _CRUNCH, _UPDATES.length);
+     _REPERTOIRE.push(data);
+     _CRUNCH += 1;
+     if (_CRUNCH != _UPDATES.length) {
+       if (_CRUNCH == (_UPDATES.length/2)) {
+         update_loading(85);
+       }
+       make_draw_repertoire();
+     } else {
+       $('#init_spin').spin(false);
+       $('#loading_text').text("");
+       crunch_data_to_useable();
+       do_animation();
+     }
+}
+
+function make_draw_repertoire() {
+  var out = [
+    {name:"command", sql:_UPDATES[_CRUNCH]}
+  ];
+  purcell.append_standard_graphical_queries(out);
+  out = {
+    client:purcell.$e$$ion$.page8.MY_NAME,
+    sql:out,
+    'return': purcell._be(purcell.$e$$ion$.page8.MY_NAME),
+    subsequent:"deposit_draw_repertoire"
+  };
+  purcell.$e$$ion$.page8.$0cket.send(JSON.stringify(out));
 }
